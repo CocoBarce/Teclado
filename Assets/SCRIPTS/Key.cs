00bdb7caff985949b0e9d140060557539c04ec3f
Cocobarce <josebarcelodawi@gmail.com>
2026-05-18 12:06:02 -0300

    commit

--- Assets/SCRIPTS/Key.cs
+++ Assets/SCRIPTS/Key.cs
@@ -6,5 +6,9 @@ public class Key : MonoBehaviour
 {
    void OnMouseDown(){
     Debug.Log("hola");
+    transform.Translate(0,0.5f,0);
+   }
+   void OnMouseUp(){
+      transform.Translate(0,-0.5f,0);
    }
 }
---
+++ Assets/SCRIPTS/KeyboardManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardManager : MonoBehaviour
+{
+    public Key[] keys;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        keys = GameObject.FindObjectsOfType<Key>();
+
+            int randomIndex = Random.Range(0, keys.Length);
+            Key randomKey = keys[randomIndex];
+
+            Destroy(randomKey.gameObject);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
